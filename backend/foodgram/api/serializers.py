7a94ae369c00cc2355250f79191f32be9b7697d6
Anton Anuchin <anton-anuchin@mail.ru>
2022-08-28 23:06:53 +0300

    foodgram

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -122,7 +122,7 @@ class RecipeWriteSerializer(serializers.ModelSerializer):
     tags = serializers.PrimaryKeyRelatedField(
         queryset=Tag.objects.all(),
         many=True)
-    image = Base64ImageField(use_url=True)
+    image = Base64ImageField(use_url=False)
 
     class Meta:
         fields = ('id', 'tags', 'author', 'ingredients', 'is_favorited',
